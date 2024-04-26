a632c0f7d313d7457247afb7451f6fe6ee1fb9b1
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2024-04-26 14:00:17 +0200

    Add service in app.js (ShoppingListCheckOffService)

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,8 @@
 
 angular.module('ShoppingListCheckOff', []);
 .controller('ToBuyController', ToBuyController);
-.controller('AlreadyBoughtController', AlreadyBoughtController);
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 function ToBuyController() {
     var toBuy = this;
@@ -12,6 +13,25 @@ function ToBuyController() {
 
 function AlreadyBoughtController() {
     var alreadyBought = this;
+}
+
+function ShoppingListCheckOffService() {
+    var service = this;
+    var itemsToBuy = [];
+    var itemsBought = [];
+
+    service.getItemsToBuy = function () {
+        return itemsToBuy;
+    }
+
+    service.getItemsBought = function () {
+        return itemsBought;
+    }
+
+    service.buyItem = function (item) {
+        itemsToBuy.splice(itemsToBuy.indexOf(item), 1);
+        itemsBought.push(item);
+    }
 }
 
 })();
