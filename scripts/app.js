7a91a1236e48f3b1010948c2a778c97c2c0da171
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2024-04-26 13:49:39 +0200

    Add 2 controllers (ToBuy and AlreadyBought)

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,16 @@
 'use strict';
 
 angular.module('ShoppingListCheckOff', []);
+.controller('ToBuyController', ToBuyController);
+.controller('AlreadyBoughtController', AlreadyBoughtController);
+
+function ToBuyController() {
+    var toBuy = this;
+
+}
+
+function AlreadyBoughtController() {
+    var alreadyBought = this;
+}
 
 })();
